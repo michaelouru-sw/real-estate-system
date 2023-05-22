318d485b702213ddaedc8b4298293acc4946bb23
Michael Ouru <39490502+michaelouru-sw@users.noreply.github.com>
2023-05-22 21:20:03 +0300

    added footer

--- client/real-estate-system/src/components/header.tsx
+++ client/real-estate-system/src/components/header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import { ColorPaletteProp } from "@mui/joy/styles";
 import Avatar from "@mui/joy/Avatar";
@@ -22,138 +23,141 @@ export default function Header() {
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const [color, setColor] = React.useState<ColorPaletteProp>("primary");
   return (
-    <Sheet
-      variant="solid"
-      color={color}
-      invertedColors
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        flexGrow: 1,
-        p: 2,
-        mx: -3,
-        my: -3,
-        borderRadius: { xs: 0, sm: "xs" },
-        minWidth: "min-content",
-        ...(color !== "warning" && {
-          background: (theme) =>
-            `linear-gradient(to top, ${theme.vars.palette[color][600]}, ${theme.vars.palette[color][500]})`,
-        }),
-      }}
-    >
-      <IconButton
-        variant="soft"
-        size="sm"
-        onClick={() => {
-          const colors: ColorPaletteProp[] = [
-            "primary",
-            "neutral",
-            "danger",
-            "info",
-            "success",
-            "warning",
-          ];
-          const nextColor = colors.indexOf(color);
-          setColor(colors[nextColor + 1] ?? colors[0]);
+    <div className="header">
+      <Sheet
+        variant="solid"
+        color="primary"
+        invertedColors
+        className="header"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexGrow: 1,
+          p: 2,
+          mx: -3,
+          my: -3,
+          borderRadius: { xs: 0, sm: "xs" },
+          minWidth: "min-content",
+          ...(color !== "warning" && {
+            background: (theme) =>
+              `linear-gradient(to top, ${theme.vars.palette[color][600]}, ${theme.vars.palette[color][500]})`,
+          }),
         }}
-        sx={{ borderRadius: "50%" }}
       >
-        <img alt="" src="/static/branding/pricing/block-gold.svg" />
-      </IconButton>
-      <Box sx={{ flex: 1, display: "flex", gap: 1, px: 2 }}>
-        <Chip
-          variant="outlined"
-          onClick={(event) => {
-            setAnchorEl(event.currentTarget);
-          }}
-          endDecorator={<KeyboardArrowDownIcon />}
-        >
-          Main
-        </Chip>
-        <Menu
-          variant="outlined"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={() => setAnchorEl(null)}
-          placement="bottom-start"
-          disablePortal
+        <IconButton
+          variant="soft"
           size="sm"
-          sx={{
-            "--ListItemDecorator-size": "24px",
-            "--ListItem-minHeight": "40px",
-            "--ListDivider-gap": "4px",
-            minWidth: 200,
+          onClick={() => {
+            const colors: ColorPaletteProp[] = [
+              "primary",
+              "neutral",
+              "danger",
+              "info",
+              "success",
+              "warning",
+            ];
+            const nextColor = colors.indexOf(color);
+            setColor(colors[nextColor + 1] ?? colors[0]);
           }}
+          sx={{ borderRadius: "50%" }}
         >
-          <MenuItem onClick={() => setAnchorEl(null)}>
-            <ListItemDecorator>
-              <BubbleChartIcon />
-            </ListItemDecorator>
-            Products
-          </MenuItem>
-          <ListDivider />
-          <MenuItem onClick={() => setAnchorEl(null)}>Pricing</MenuItem>
-          <MenuItem onClick={() => setAnchorEl(null)}>
-            Case studies{" "}
-            <Chip
-              variant="outlined"
-              size="sm"
-              sx={{
-                ml: "auto",
-                bgcolor: (theme) =>
-                  `rgba(${theme.vars.palette.primary.mainChannel} / 0.1)`,
-              }}
-            >
-              Beta
-            </Chip>
-          </MenuItem>
-        </Menu>
-      </Box>
-      <Box
-        sx={{ display: "flex", flexDirection: "row-reverse", flexShrink: 0 }}
-      >
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <Button
-            startDecorator={<AddIcon />}
+          <img alt="" src="/static/branding/pricing/block-gold.svg" />
+        </IconButton>
+        <Box sx={{ flex: 1, display: "flex", gap: 1, px: 2 }}>
+          <Chip
+            variant="outlined"
+            onClick={(event) => {
+              setAnchorEl(event.currentTarget);
+            }}
+            endDecorator={<KeyboardArrowDownIcon />}
+          >
+            Main
+          </Chip>
+          <Menu
+            variant="outlined"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={() => setAnchorEl(null)}
+            placement="bottom-start"
+            disablePortal
+            size="sm"
             sx={{
-              borderRadius: "xl",
-              display: { xs: "none", md: "inline-flex" },
+              "--ListItemDecorator-size": "24px",
+              "--ListItem-minHeight": "40px",
+              "--ListDivider-gap": "4px",
+              minWidth: 200,
             }}
           >
-            New Invoice
-          </Button>
-          <Input
-            placeholder="Search for anything..."
-            variant="soft"
-            size="sm"
-            endDecorator={
-              <Typography
-                component="span"
+            <MenuItem onClick={() => setAnchorEl(null)}>
+              <ListItemDecorator>
+                <BubbleChartIcon />
+              </ListItemDecorator>
+              Products
+            </MenuItem>
+            <ListDivider />
+            <MenuItem onClick={() => setAnchorEl(null)}>Pricing</MenuItem>
+            <MenuItem onClick={() => setAnchorEl(null)}>
+              Case studies{" "}
+              <Chip
                 variant="outlined"
-                level="body3"
-                sx={{ bgcolor: "background.surface", mx: 0 }}
+                size="sm"
+                sx={{
+                  ml: "auto",
+                  bgcolor: (theme) =>
+                    `rgba(${theme.vars.palette.primary.mainChannel} / 0.1)`,
+                }}
               >
-                ⌘K
-              </Typography>
-            }
-            sx={{
-              "--Input-radius": "40px",
-              "--Input-paddingInline": "12px",
-              width: 160,
-              display: { xs: "none", lg: "flex" },
-            }}
-          />
-          <Badge badgeContent={2} variant="solid" color="danger">
-            <IconButton variant="soft" sx={{ borderRadius: "xl" }}>
-              <NotificationsIcon />
-            </IconButton>
-          </Badge>
-          <IconButton sx={{ borderRadius: "xl" }}>
-            <Avatar src="/static/images/avatar/1.jpg" size="sm" />
-            <KeyboardArrowDownIcon />
-          </IconButton>
+                Beta
+              </Chip>
+            </MenuItem>
+          </Menu>
         </Box>
-      </Box>
-    </Sheet>
+        <Box
+          sx={{ display: "flex", flexDirection: "row-reverse", flexShrink: 0 }}
+        >
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Button
+              startDecorator={<AddIcon />}
+              sx={{
+                borderRadius: "xl",
+                display: { xs: "none", md: "inline-flex" },
+              }}
+            >
+              New Home
+            </Button>
+            <Input
+              placeholder="Search for anything..."
+              variant="soft"
+              size="sm"
+              endDecorator={
+                <Typography
+                  component="span"
+                  variant="outlined"
+                  level="body3"
+                  sx={{ bgcolor: "background.surface", mx: 0 }}
+                >
+                  ⌘K
+                </Typography>
+              }
+              sx={{
+                "--Input-radius": "40px",
+                "--Input-paddingInline": "12px",
+                width: 160,
+                display: { xs: "none", lg: "flex" },
+              }}
+            />
+            <Badge badgeContent={2} variant="solid" color="danger">
+              <IconButton variant="soft" sx={{ borderRadius: "xl" }}>
+                <NotificationsIcon />
+              </IconButton>
+            </Badge>
+            <IconButton sx={{ borderRadius: "xl" }}>
+              <Avatar src="/static/images/avatar/1.jpg" size="sm" />
+              <KeyboardArrowDownIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </Sheet>
+    </div>
   );
 }
